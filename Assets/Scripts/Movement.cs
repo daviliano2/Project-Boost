c8eb53cce162f297037603fdcaef2c01cf4f6632
daviliano2 <daviliano2@gmail.com>
2021-03-18 19:54:17 +0100

    35. Add some inputs
    Add movement script to rocket object.
    Add methods for thrust and rotation and log when a-d-space keys are pressed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,33 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetThrust();
+        GetRotation();
+    }
+
+    void GetThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("pressing space");
+        }
+    }
+
+    void GetRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("pressing A");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("pressing D");
+        }
     }
 }
