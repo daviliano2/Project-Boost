2214c9de16c16a63528ce9700ff68692a9b27f86
daviliano2 <daviliano2@gmail.com>
2021-04-06 21:22:59 +0200

    45. load next leve
    Add logic to cycle through levels

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,14 +8,30 @@ public class CollisionHandler : MonoBehaviour
         switch (other.gameObject.tag)
         {
             case "Start":
+                Debug.Log("collided with friendly object, all good");
+                break;
             case "Finish":
                 Debug.Log("collided with friendly object, all good");
+                LoadNextLevel();
                 break;
             default:
                 Debug.Log("Collided with something bad, U ded");
                 ReloadLevel();
                 break;
         }
+    }
+
+    void LoadNextLevel()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     void ReloadLevel()
